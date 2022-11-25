e9a14c0c4a4ff8723fe16267c443d3437549bd9e
ana-lapas <anapaulalapas@gmail.com>
2022-11-24 20:29:03 -0400

    Feat Products List

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ import PerfilPage from "./components/PerfilPage.js"
 import GlobalStyle from "./styles/globalStyles.js"
 import { useState } from "react"
 import UserContext from "./contexts/UserContext.js"
+import ProductsListPage from "./components/ProductListPage.js"
+import KartContext from "./contexts/KartContext.js"
 
 function App() {
   const [token, setToken] = useState("")
   const [name, setName] = useState("")
-
+  const [kart, setKart] = useState([])
   return (
     <UserContext.Provider value={token}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/perfil" element={<PerfilPage />} />
-        </Routes>
-      </BrowserRouter>
+      <KartContext.Provider value={{ kart, setKart }}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/home" element={<ProductsListPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/perfil" element={<PerfilPage />} />
+          </Routes>
+        </BrowserRouter>
+      </KartContext.Provider>
     </UserContext.Provider>
   )
 }
